65041f1fecf31286c2714a3c42cc47c908707cb3
apfdamascena <apfd@cin.ufpe.br>
2021-06-16 08:18:58 -0300

    feature(event-section): fix run problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import InitialScetion from "./components/Sections/InitialSection";
 import { GlobalStyle } from "./styles/global.js";
 import AboutUs from "./components/Sections/AboutUs";
 import Event from "./components/Sections/Event";
-import CarouselItem from "./components/CarouselItem";
 
 function App() {
   return (
