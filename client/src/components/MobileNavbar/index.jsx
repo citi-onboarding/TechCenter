64d5dabc02eca4b5739d45953917ed2512d3e801
phsb5321 <phsb5321@gmail.com>
2021-06-23 17:24:42 -0300

    feaure(mobile-navbar): ant design wont work for the dropwdown

--- client/src/components/MobileNavbar/index.jsx
+++ client/src/components/MobileNavbar/index.jsx
@@ -1,13 +1,37 @@
-import React, { useState } from 'react'
+import { Button, Dropdown, Menu, Space } from 'antd';
+import React, { useState } from 'react';
 
 import { Container } from "./styles.js";
 import logo from "../../assets/mobileNavbarLogo.svg"
 
 export default function MobileNavbar() {
 
+    const menu = (
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+                    1st menu item
+                </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+                    2nd menu item
+                </a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+                    3rd menu item
+                </a>
+            </Menu.Item>
+        </Menu>
+    );
+
     return (
         <Container>
             <img src={logo} alt="mobile navbar logo" />
+            <Dropdown overlay={menu} placement="bottomRight">
+                <Button>bottomRight</Button>
+            </Dropdown>
         </Container>
     )
 }
--- client/src/components/MobileNavbar/styles.js
+++ client/src/components/MobileNavbar/styles.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.nav`
+    @media(min-width: 1200px){
+        &{
+            display: none;
+        }
+    }
 
+    background-color: var(--FIRST-PRIMARY-COLOR);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    img{
+        margin: 5px;
+    }
 `
