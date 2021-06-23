ed775c591a8496efb0c7a7e0f87771552279304b
phsb5321 <phsb5321@gmail.com>
2021-06-23 17:49:40 -0300

    feaure(mobile-navbar): material ui implemented

--- client/src/components/MobileNavbar/index.jsx
+++ client/src/components/MobileNavbar/index.jsx
@@ -1,37 +1,44 @@
-import { Button, Dropdown, Menu, Space } from 'antd';
 import React, { useState } from 'react';
 
+import Button from '@material-ui/core/Button';
 import { Container } from "./styles.js";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ham from "../../assets/hamburguer.svg"
 import logo from "../../assets/mobileNavbarLogo.svg"
 
 export default function MobileNavbar() {
 
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-                    1st menu item
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-                    2nd menu item
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-                    3rd menu item
-                </a>
-            </Menu.Item>
-        </Menu>
-    );
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <Container>
             <img src={logo} alt="mobile navbar logo" />
-            <Dropdown overlay={menu} placement="bottomRight">
-                <Button>bottomRight</Button>
-            </Dropdown>
+
+            <div>
+                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                    <img src={ham} alt="menu hamburguer" />
+                </Button>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+            </div>
         </Container>
     )
 }
