546ff43814861d5f8bd62cd86547d8f4d4a3a4b4
apfdamascena <apfd@cin.ufpe.br>
2021-06-24 23:55:21 -0300

    hotfix(final-fixes): pushing again

--- client/src/components/Sections/AboutUs/styles.js
+++ client/src/components/Sections/AboutUs/styles.js
@@ -146,7 +146,7 @@ export const ValuesAboutUsContainer = styled.div`
             }
         }
 
-        &.mid-values{
+        &.mid-values {
             background: var(--WHITE-BACKGROUND);
             color: var(--FIRST-PRIMARY-COLOR);
             box-shadow: none;
