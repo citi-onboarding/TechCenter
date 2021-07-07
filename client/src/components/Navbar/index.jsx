70dfe7901663314895ca6de7f9e93325c4ce58af
phsb5321 <phsb5321@gmail.com>
2021-07-07 14:47:11 -0300

    feature(navbar-made-simple):navbar has no more js logic in it

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,33 +1,11 @@
-import React, { useState } from 'react'
-
 import { Container } from "./styles.js";
+import React from 'react'
 import logo from "../../assets/small-logo-navbar.png"
 
 export default function Navbar() {
 
-    const [isOnTop, setIsOnTop] = useState(false)
-    const [box, setBox] = useState()
-
-    window.onscroll = function () {
-        myFunction()
-        setBox(document.querySelector("#myHeader"))
-    };
-
-    function myFunction() {
-        if (box) {
-            const compHeight = box.offsetHeight
-            const height = window.innerHeight
-            if (window.pageYOffset <= height - compHeight) {
-                setIsOnTop(false)
-            } else {
-                setIsOnTop(true)
-            }
-            console.log(`Is it on top ? ${isOnTop}`)
-        }
-    }
-
     return (
-        <Container top={isOnTop} id="myHeader">
+        <Container>
             <ul>
                 <li>
                     <a href="#FIRST"><img src={logo} alt="small-logo-navbar" /></a>
